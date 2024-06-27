f247c69566c83c8b2a38004ed7f97b190515181d
Kavan Thakkar <95115101+Ka1Thakkar@users.noreply.github.com>
2024-06-27 17:03:50 +0530

    Removed pauseOnHover

--- app/_components/Insights.tsx
+++ app/_components/Insights.tsx
@@ -15,7 +15,7 @@ const Insights = () => {
                     See why investors choose dealnudge for superior deal flow
                 </p>
             </div>
-            <Marquee pauseOnHover className="w-screen h-fit" gradient gradientColor={theme === "dark" ? "#000000" : "#FFFFFF"} gradientWidth={"25%"}>
+            <Marquee className="w-screen h-fit" gradient gradientColor={theme === "dark" ? "#000000" : "#FFFFFF"} gradientWidth={"25%"}>
                 <InsightsComponent image={image} name="Kavan T." position="Venture Capital Associate" insights="Dealnudge has been a game changer for our team. We have been able to source deals faster and more efficiently than ever before." />
                 <InsightsComponent image={image} name="Kavan T." position="Venture Capital Associate" insights="Dealnudge has been a game changer for our team. We have been able to source deals faster and more efficiently than ever before." />
                 <InsightsComponent image={image} name="Kavan T." position="Venture Capital Associate" insights="Dealnudge has been a game changer for our team. We have been able to source deals faster and more efficiently than ever before." />
